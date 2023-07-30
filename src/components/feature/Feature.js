0f51963f717005aad549bce55fff21b1a9dd4c7b
KratikSoni1104 <118619980+KratikSoni1104@users.noreply.github.com>
2023-07-30 23:52:43 +0530

    added images

--- src/components/feature/Feature.js
+++ src/components/feature/Feature.js
@@ -13,7 +13,7 @@ const Featured = () => {
         <>
           <div className="featuredItem">
             <img
-              src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+              src="images/Evening_view,_City_Palace,_Udaipur.jpg"
               alt=""
               className="featuredImg"
             />
@@ -25,7 +25,7 @@ const Featured = () => {
 
           <div className="featuredItem">
             <img
-              src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
+              src="images/pexels-rishabh-dhiman-13471509.jpg"
               alt=""
               className="featuredImg"
             />
@@ -34,9 +34,9 @@ const Featured = () => {
               <h2>{data[1]} properties</h2>
             </div>
           </div>
-          <div className="featuredItem">
+          <div className="featuredItem jaipur">
             <img
-              src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
+              src="images/pexels-poonam-sharma-6185650.jpg"
               alt=""
               className="featuredImg"
             />
